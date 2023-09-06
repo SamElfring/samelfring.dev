b53f2b3030b73bb71278d1d5fd2d608a9961419e
Sam <elfringsam@gmail.com>
2023-09-06 17:17:27 +0200

    Added Font Awesome Brand icons

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, fab);
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
