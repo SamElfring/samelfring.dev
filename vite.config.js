48503526892d667a53f660327bcbdfa28bef0a86
Sam <elfringsam@gmail.com>
2023-09-06 16:48:31 +0200

    Added Google Fonts plugin

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const GoogleFontsPlugin = require("@beyonk/google-fonts-webpack-plugin");
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -13,5 +15,14 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-    base: "/samelfring.nl"
+    base: "/samelfring.nl",
+    configureWebpack: {
+        plugins: [
+            new GoogleFontsPlugin({
+                fonts: [
+                    { family: "Poppins", variants: [ "500", "700" ] }
+                ]
+            })
+        ]
+    }
 });
