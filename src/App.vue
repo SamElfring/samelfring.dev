2d6bb1f35ce1e6b49cf9e46b592ee3b1b9f7eb7e
Sam <elfringsam@gmail.com>
2023-09-13 22:58:58 +0200

    Added Experiences section

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
         <AboutMe class="mt-8" />
 
         <Study class="mt-4" />
+
+        <Experiences class="mt-4" />
     </div>
 
     <Footer />
@@ -17,5 +19,6 @@ import Header from "./components/Header.vue";
 import Hero from "./components/Hero.vue";
 import AboutMe from "./components/about/AboutMe.vue";
 import Study from "./components/Study.vue";
+import Experiences from "./components/Experiences.vue";
 import Footer from "./components/Footer.vue";
 </script>
---
+++ src/components/Experiences.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p class="w-max font-bold border-b-2 border-slate-400 mb-4">
+            Ervaringen
+        </p>
+
+        <Timeline :items="timeLineItems" />
+    </div>
+</template>
+
+<script setup>
+import Timeline from "./helpers/Timeline.vue";
+
+const timeLineItems = [
+    {
+        time: "2022 - nu",
+        title: "Software Developer bij Camping Care",
+        text: "Ik werk nu bij Camping.care in Denekamp waar ik samen \
+                met een klein team werk aan een PMS voor campings \
+                dat wordt gebruikt over heel Europa."
+    },
+    {
+        time: "2020",
+        title: "Stage bij Cube B.V.",
+        text: "Meegedraaid in het team en gewerkt aan verschillende \
+                projecten waaronder het ontwikkelen van een website \
+                waarmee heel eenvoudig Cookie meldingen worden \
+                gegenereerd."
+    },
+    {
+        time: "2018",
+        title: "Meeloopdagen bij Telecats B.V. en Conceptlisious",
+        text: "Tijdens deze meeloopdagen heb ik meerdere SCRUM sessies \
+                bijgewoond en ben ik bezig geweest met het testen van \
+                virtual reality spellen."
+    }
+];
+</script>
