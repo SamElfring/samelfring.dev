f74be9779a9bc1e591f6b26c30d2bd28ff4b52c9
Sam <elfringsam@gmail.com>
2023-09-22 20:07:32 +0200

    Added header navigation links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,31 @@
 <template>
-    <nav class="flex items-center">
-        <div class="bg-blue-600 px-3 py-1 mr-2 rounded-full font-black text-lg">S</div>
-        <h2 class="text-lg font-bold">Sam Elfring</h2>
+    <nav class="flex justify-between items-center">
+        <div class="flex items-center">
+            <div class="bg-blue-600 px-3 py-1 mr-2 rounded-full font-black text-lg">S</div>
+            <h2 class="text-lg font-bold">Sam Elfring</h2>
+        </div>
+
+        <ul class="hidden md:flex gap-4">
+            <li
+                v-for="link in navLinks"
+            >
+                <a
+                    :href="link.link"
+                    class="text-slate-400 hover:underline hover:text-white"
+                >
+                    {{ link.name }}
+                </a>
+            </li>
+        </ul>
     </nav>
 </template>
+
+<script setup>
+const navLinks = [
+    { name: "Over mij", link: "#about-me" },
+    { name: "Ervaringen", link: "#experiences" },
+    { name: "Opleidingen", link: "#study" },
+    { name: "Projecten", link: "#projects" },
+    { name: "Vaardigheden", link: "#skills" }
+];
+</script>
