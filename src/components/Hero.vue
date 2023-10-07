52cc62e81a8b859f673053574ff7c97b76e1cfee
Sam <elfringsam@gmail.com>
2023-10-07 23:18:09 +0200

    Renamed cv file

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -18,7 +18,7 @@
 
             <div class="mt-4 text-slate-300">
                 <a
-                    href="/files/cv_Sam Elfring.pdf"
+                    href="/files/cv_Sam_Elfring.pdf"
                     download
                     class="mr-4 border-blue-500 border px-4 py-2 rounded-full shadow-2xl hover:bg-blue-500 hover:text-white hover:animate-pulse"
                 >
