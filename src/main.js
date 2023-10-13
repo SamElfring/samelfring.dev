52ab143a7746785d07787d8890e09965849ffdb1
Sam <elfringsam@gmail.com>
2023-10-13 14:56:46 +0200

    Added Vue i18n

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// i18n
+import { createI18n } from "vue-i18n";
+
+// Translations
+import en from "./locales/en.json";
+import nl from "./locales/nl.json";
+
+const i18n = createI18n({
+    locale: "nl",
+    messages: { en, nl }
+});
+
 // Font Awesome
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -12,6 +24,7 @@ library.add(fas, fab);
 
 const app = createApp(App);
 
+app.use(i18n);
 app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
