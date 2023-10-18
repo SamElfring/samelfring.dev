6f0c722353d0674e0de4e3ecb533b473242d19fc
Sam Elfring <elfringsam@gmail.com>
2023-10-18 17:36:24 +0200

    Moved CV download to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,39 +1,15 @@
 <template>
-    <div class="p-4 bg-slate-800 border-t-2 border-t-slate-700 text-center">
-        <div class="flex justify-center gap-4">
-            <a
-                v-for="link in footerLinks"
-                :href="link.link"
-                target="_blank"
-                :aria-label="link.label"
-            >
-                <font-awesome-icon
-                    :icon="link.icon"
-                    class="text-xl"
-                ></font-awesome-icon>
-            </a>
-        </div>
+    <div class="p-4 flex justify-center gap-2 bg-slate-800 border-t-2 border-t-slate-700 text-center">
+        <p class="text-slate-300">&copy; {{ new Date().getFullYear() }} - Sam Elfring</p>
 
-        <p class="mt-2 text-slate-300">&copy; {{ new Date().getFullYear() }} - Sam Elfring</p>
+        <p>&#x2022;</p>
+
+        <a
+            href="/files/cv_Sam_Elfring.pdf"
+            download
+            class="text-slate-300 hover:underline"
+        >
+            Download CV
+        </a>
     </div>
 </template>
-
-<script setup>
-const footerLinks = [
-    {
-        icon: "fa-brands fa-facebook",
-        link: "https://www.facebook.com/sam.elfring.3/",
-        label: "Navigate to Facebook page"
-    },
-    {
-        icon: "fa-brands fa-instagram",
-        link: "https://www.instagram.com/sam_elfring/",
-        label: "Navigate to Instagram page"
-    },
-    {
-        icon: "fa-brands fa-linkedin",
-        link: "https://www.linkedin.com/in/sam-elfring-061822194/",
-        label: "Navigate to LinkedIn page"
-    }
-];
-</script>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -14,14 +14,6 @@
             </p>
 
             <div class="mt-4 text-slate-300 flex items-center">
-                <a
-                    href="/files/cv_Sam_Elfring.pdf"
-                    download
-                    class="mr-4 border-blue-500 border px-4 py-2 rounded-full shadow-2xl hover:bg-blue-500 hover:text-white hover:animate-pulse"
-                >
-                    Download CV
-                </a>
-
                 <div class="flex bg-slate-800 rounded-lg overflow-hidden">
                     <a
                         v-for="link in socialMediaLinks"
