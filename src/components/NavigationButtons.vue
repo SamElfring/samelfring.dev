a59d7a2d32ec785adbaff598f6252fcf8ea17bf6
Sam Elfring <elfringsam@gmail.com>
2023-10-18 18:24:51 +0200

    Updated NavigationButtons

--- src/components/NavigationButtons.vue
+++ src/components/NavigationButtons.vue
@@ -2,25 +2,36 @@
     <section class="grid grid-cols-2 gap-4 md:hidden">
         <div
             v-for="item in navItems"
-            :key="item[0]"
             class="flex justify-center items-center border-2 border-slate-700 rounded-lg hover:border-blue-700 bg-gradient-to-r from-slate-900 to-slate-800"
         >
             <a
-                :href="item[0]"
+                :href="item.link"
                 class="font-semibold w-full text-center py-2"
-                :aria-label="item[2]"
+                :aria-label="$t(item.text)"
             >
-                {{ $t(item[1]) }}
+                {{ $t(item.text) }}
             </a>
         </div>
     </section>
 </template>
 
 <script setup>
-const navItems = [
-    ["#about-me", "main.aboutMe", "Navigate to about me section"],
-    ["#study", "main.studies", "Navigate to studies section"],
-    ["#experiences", "main.experiences", "Navigate to experiences section"],
-    ["#projects", "main.projects", "Navigate to projects section"]
-];
+const navItems = {
+    aboutMe: {
+        link: "#about-me",
+        text: "main.aboutMe"
+    },
+    study: {
+        link: "#study",
+        text: "main.studies"
+    },
+    experiences: {
+        link: "#experiences",
+        text: "main.experiences"
+    },
+    projects: {
+        link: "#projects",
+        text: "main.projects"
+    }
+};
 </script>
