2bd07b19a72a9042390e49a81f3c7c8e6c148c13
SamElfring <elfringsam@gmail.com>
2024-01-22 22:42:27 +0100

    Added not found page

---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+    <Header />
+
+    <div class="flex flex-col justify-center items-center h-screen">
+        <img
+            src="/img/404.webp"
+            alt="404"
+        />
+
+        <h1 class="text-5xl text-slate-200">{{ $t("main.not_found").toUpperCase() }}</h1>
+
+        <NuxtLink
+            to="/"
+            class="mt-6 px-4 py-2 rounded-md bg-slate-700 text-slate-200 hover:bg-slate-500"
+        >
+            {{ $t("main.go_home") }}
+        </NuxtLink>
+    </div>
+</template>
