f2512e28bd7b0f87eb335bde3bd3b455e3aaa97e
SamElfring <elfringsam@gmail.com>
2024-01-22 22:50:50 +0100

    Added link to repository in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="p-4 flex justify-center gap-2 bg-slate-800 border-t-2 border-t-slate-700 text-center">
         <p class="text-slate-300">&copy; {{ new Date().getFullYear() }} - Sam Elfring</p>
+
+        <p>&#x2022;</p>
+
+        <a
+            href="https://github.com/SamElfring/samelfring.nl"
+            target="_blank"
+            class="text-slate-300 hover:underline"
+            aria-label="Source code"
+        >
+            Source code
+        </a>
     </div>
 </template>
