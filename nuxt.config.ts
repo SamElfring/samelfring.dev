41e230abca270c676a5e6ec710916a320ebcc173
Sam Elfring <elfringsam@gmail.com>
2024-03-05 13:59:19 +0100

    Added title and description meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,15 @@ export default defineNuxtConfig({
     },
     app: {
         head: {
+            title: "Sam Elfring - Software Developer",
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content:
+                        "I am Sam, a 20-year-old Software Developer with a passion for creating digital solutions. I study at Saxion in the Software Development program."
+                }
+            ],
             script: [
                 { src: "https://www.googletagmanager.com/gtag/js?id=G-D32TP1BEDD", async: true },
                 { src: "js/analytics.js" }
