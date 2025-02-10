e1146875c8fcfb4810a3f69022cbcd7afacd1316
Sam Elfring <elfringsam@gmail.com>
2025-02-10 18:38:45 +0100

    Updated dialog design

--- components/ProjectPopup.vue
+++ components/ProjectPopup.vue
@@ -1,23 +1,40 @@
 <template>
-    <div class="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
-        <div class="bg-slate-900 p-4 rounded-lg max-w-3xl w-full">
-            <button
-                @click="emit('close')"
-                class=""
+    <div
+        @click="emit('close')"
+        class="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+    >
+        <div
+            @click.stop
+            class="bg-slate-900 rounded-lg max-w-3xl w-full border border-slate-600 overflow-hidden"
+        >
+            <div
+                class="bg-center bg-cover bg-no-repeat h-96 p-2 relative flex items-end border-b border-slate-600"
+                :style="{ backgroundImage: `url('${props.project.img}')` }"
             >
-                X
-            </button>
+                <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-black"></div>
 
-            <h2 class="text-2xl font-bold mb-4">{{ $t(project.title) }}</h2>
+                <div class="z-10 p-2 flex justify-between items-center w-full">
+                    <p class="font-bold text-lg">{{ $t(props.project.title) }}</p>
 
-            <div class="grid gap-4 grid-cols-1 md:grid-cols-2">
-                <img
-                    :src="props.project.img"
-                    class="w-full h-auto"
-                />
+                    <a
+                        v-for="url in props.project.urls"
+                        :key="url[1]"
+                        :href="url[1]"
+                        target="_blank"
+                        class="flex gap-2"
+                        :aria-label="url[2]"
+                    >
+                        <font-awesome-icon
+                            :icon="url[0]"
+                            class="text-2xl rounded-full"
+                        ></font-awesome-icon>
+                    </a>
+                </div>
             </div>
 
-            <p class="mt-4">{{ $t(project.description) }}</p>
+            <div class="flex-1 p-2 bg-slate-800">
+                <p class="text-slate-300">{{ $t(props.project.description) }}</p>
+            </div>
         </div>
     </div>
 </template>
